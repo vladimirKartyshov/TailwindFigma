807d92d02a7529636cec395bc01fe4b36d7c13f1
vladimirKartyshov <vovan198080"inbox.ru>
2021-05-30 15:40:51 +0300

    taikwindconfig changed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// module.exports = {
+//     theme: {
+//         backgroundColor: theme => ({
+//             ...theme('color'),
+//             'color' : '#66c05d',
+//             // 'primary': '#3490dc',
+//             // 'secondary': '#ffed4a',
+//             // 'danger': '#e3342f',
+//         })
+//     }
+// }
